c558b7efc0a82c4d0c9afe4a70ad24ae17434014
Jacob Stenson <jacob@nosnets.co.uk>
2019-03-03 15:53:15 +0000

    Update Entry.php
    Removed unnecessary code.

--- DatabasePHP/Entry.php
+++ DatabasePHP/Entry.php
@@ -14,11 +14,7 @@
       }
       
       $nfc_id = $_GET['nfcid'];
-      echo $nfc_id;
-      echo "<br>";
       $student_id = $_GET['studid'];
-      echo $student_id;
-      echo "<br>";
       
       $sql = 'SELECT NFCID FROM studentdata'; 
       $result = mysqli_query($conn, $sql);
@@ -43,13 +39,6 @@
       {
       echo 'You are not in the database';
       }
-      
-      
-      
-      # check if the nfccode is in the database  
-      # check if the student has a lesson in the room and at the time
-      # add 1 fo the attendance section (present, late, absent)
-      # 
     ?>
   </body>
 </html>
