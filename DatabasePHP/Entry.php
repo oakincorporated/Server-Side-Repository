de7236d2551e3a0f5d4a4161dae3432c6600e241
ElliotHawkins <35804995+ElliotHawkins@users.noreply.github.com>
2019-03-06 16:08:44 +0000

    Update Entry.php
    Checks the student id and whether the student has a class on the day. If this is true ever users present column on attendance_t is increased by 1.

--- DatabasePHP/Entry.php
+++ DatabasePHP/Entry.php
@@ -1,7 +1,4 @@
 <!DOCTYPE html>
-<html>
-  <head>
-  </head>
   <body>
     <?php
 
@@ -9,52 +6,99 @@
       $username = "root";
       $password = "";
       $dbname = "realworldproject";
-
+	  
       $conn = new mysqli($servername, $username, $password, $dbname);
       if (!$conn)
       {
         die("Connection fail.". mysqli_connect_error());
       }
-
-
-    if isset($_GET['nfcid'])
-    {
-       $nfc_id = $_GET['nfcid'];
-    }else{
-        die();
-      }
-
-    if isset($_GET['studid'])
-    {
+      
+	  date_default_timezone_set('Europe/London');	#TIME STAMPS
+      $scanTime = date("H:i:s");
+	  echo($scanTime . "<br>");
+      $scanDate = date("D jS F Y");
+	  
+	  $currentday = date("N");
+	  # echo($currentday . "<br>");
+      echo($scanDate . "<br>");
+	  
+	  
+      $nfc_id = $_GET['nfcid'];		# Parameters
+      echo $nfc_id;
+      echo "<br>";
       $student_id = $_GET['studid'];
-    }else{
-        die();
-      }
-
-
-      $sql = 'SELECT NFCID FROM studentdata';
+      echo $student_id;
+      echo "<br>";
+      
+	  
+	  
+      $sql = 'SELECT NFC_ID FROM student_t'; 	# SQL queries for NFC_ID
       $result = mysqli_query($conn, $sql);
-
+      
       while($row = mysqli_fetch_assoc($result)){
-        $currentNFCFound = $row['NFCID'];
+        $currentNFCFound = $row['NFC_ID'];
+		
         if($currentNFCFound == $nfc_id){
-          //echo 'yes';
           $nfcWasFound = true;
           break;
         }else{
-          //echo 'no';
           $nfcWasFound = false;
         }
-
+      
       }
-
+      
       if($nfcWasFound){
-        echo 'You are in the database';
-      }
-      else
-      {
-      echo 'You are not in the database';
-      }
+        echo 'you are in the database <br>';
+		
+		
+		
+		$sql = 'SELECT Student_ID FROM student_t'; # check for the student id
+		$result = mysqli_query($conn, $sql);
+		
+		while($row = mysqli_fetch_assoc($result)){
+			$studentID = $row['Student_ID'];
+			if($student_id == $studentID){
+				$isCorrect = true;
+				break;
+			}
+			else{
+				$isCorrect = false;
+			}
+			}
+		}
+		
+		if ($isCorrect){
+			echo"you student ID is correct <br>";
+			$sql = 'SELECT Day FROM class_t'; # check if the student has a lesson on this day
+			$result = mysqli_query($conn, $sql);
+			
+			while($row = mysqli_fetch_assoc($result)){
+				$classDay = $row['Day'];
+			
+				if ($classDay == $currentday){
+					$todayclass = true;
+					break;
+			}
+				else{
+					$todayclass = false; 
+				}
+			}
+			}
+			if($todayclass){
+				echo'you have a class today <br>';
+				$sql = 'UPDATE attendance_t SET Present = Present + 1';
+				mysqli_query($conn, $sql);
+				echo'you attendance has been updated';
+				
+			}
+			else{
+				echo'you dont have a class today';
+			}
+	  
+	  
+	  
+      # add 1 fo the attendance section (present, late, absent)
+      # 
     ?>
   </body>
 </html>
