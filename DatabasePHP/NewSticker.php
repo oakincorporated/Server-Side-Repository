ca8c384272e5a2fe360d4999fc8b565035c4d3ec
andreibiv <48105170+andreibiv@users.noreply.github.com>
2019-03-10 22:15:55 +0000

    Updated the file to work "properly"- needs testing
    Put the main SQL fetch and update in the PHP block. Added a header and another echo statement in the HTML block. These needs testing and I will, once I get around running XAMPP on a machine. Plus, I am using a `nfc_empty_tags` table that currently is not on the main SQL code on GitHub.

--- DatabasePHP/NewSticker.php
+++ DatabasePHP/NewSticker.php
@@ -15,39 +15,55 @@
   }
   $sql = "SELECT Username, NFCID FROM studentdata";
   $result = mysqli_query($conn, $sql);
+  
+  $currID = $mysqli->prepare("SELECT NFCID FROM studentdata WHERE username = ?"); //grabs the current NFC ID
+  $currID -> bind_param("s", $_POST[username]);
+  $currID -> execute();
+
+  $currSID = mysqli->prepare("SELECT StudentID FROM studentdata WHERE username = ?"); //grabs the current Student ID
+  $currSID -> bind_param("s", $_POST[username]);
+  $currSID -> execute();
+
+  
+  //unsure about the stuff below: .....
+  
+  $newNFC = $mysqli->prepare("SELECT NFCID FROM nfc_empty_tags WHERE postition LIKE '%?"); //grabs a new NFC ID from the table; takes the one whose 'position' is the same as the last digit of the Student ID, in order not to create confusion
+  $newNFC -> bind_param("s", $_POST[currSID];
+  $newNFC -> execute();
+   
+  //unsure about the stuff above: ....
+  
+  
+  $updateSql = ("UPDATE studentdata SET NFCID = %s WHERE StudentID = %s", $newNFC, $currSID);
+  $updateSql -> bind_param("ii", $_POST[newNFC], $_POST[currSID]);
+  $currSID -> execute();
+  
+  $fetchupdatesql = "SELECT Username, NFCID FROM studentdata";
+  $fetchupdatesql = mysqli_query($conn, $sql);
+  
  ?>
 
- $currID = $mysqli->prepare("SELECT NFCID FROM studentdata WHERE username = ?");
- $currID -> bind_param("s", $_POST[username]);
- $currID -> execute(); 	
- 
- 
- $currSID = mysqli->prepare("SELECT StudentID FROM studentdata WHERE username = ?");
- $currSID -> bind_param("s", $_POST[username]);
- $currSID -> execute();
- 
- $newID = newNFCID()
- 
- $updateSql = ("UPDATE studentdata SET NFCID = %s WHERE StudentID = %s", $newID, $currSID);
- $updateSql -> bind_param("ii", $_POST[newID], $_POST[currSId]);
- $currSID -> execute();
-
- 
----
 
  
  
  
  <!DOCTYPE html>
  <html>
- <head></head>
+ <head>Replacing the NFC tag.</head>
  <body>
    <?php if(mysqli_num_rows($result) > 0)
    {
      // Show data for each row
      while($row = mysqli_fetch_assoc($result))
      {
-       echo "|The student whose NFCID needs replacement: ".$row['Username']."|The NFCID to be replaced:".$row['NFCID'].";";
+       echo "|The student whose NFCID needs replacement: ".$row['Username']."|The NFCID to be replaced:".$row['NFCID']."; \n";
+     }
+	 
+	 <!-- Unsure about this bit below too, has to be tested -->
+	 
+	 while($row = mysqli_fetch_assoc($fetchupdatesql))
+     {
+       echo "|There was a new NFC ID update to the student with the SID: ".$row['Username']."|The new NFC tag code assigned :".$row['NFCID']."; \n";
      }
    } ?>
  </body>
