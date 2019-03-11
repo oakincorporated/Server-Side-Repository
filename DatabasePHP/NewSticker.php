0eceb1d3718155876c91da98c7d20c3a9a57f083
andreibiv <48105170+andreibiv@users.noreply.github.com>
2019-03-11 14:28:36 +0000

    Update NewSticker.php
    Removed some comments and re-located some code blocks.

--- DatabasePHP/NewSticker.php
+++ DatabasePHP/NewSticker.php
@@ -18,25 +18,31 @@
   
   $currID = $mysqli->prepare("SELECT NFCID FROM studentdata WHERE username = ?"); //grabs the current NFC ID
   $currID -> bind_param("s", $_POST[username]);
-  $currID -> execute();
 
-  $currSID = mysqli->prepare("SELECT StudentID FROM studentdata WHERE username = ?"); //grabs the current Student ID
+
+  $currSID = $mysqli->prepare("SELECT StudentID FROM studentdata WHERE username = ?"); //grabs the current Student ID
   $currSID -> bind_param("s", $_POST[username]);
-  $currSID -> execute();
 
   
-  //unsure about the stuff below: .....
+  //TODO: test this function for every option
   
   $newNFC = $mysqli->prepare("SELECT NFCID FROM nfc_empty_tags WHERE postition LIKE '%?"); //grabs a new NFC ID from the table; takes the one whose 'position' is the same as the last digit of the Student ID, in order not to create confusion
-  $newNFC -> bind_param("s", $_POST[currSID];
-  $newNFC -> execute();
-   
-  //unsure about the stuff above: ....
+  $newNFC -> bind_param("s", $_POST[currSID]);
   
   
-  $updateSql = ("UPDATE studentdata SET NFCID = %s WHERE StudentID = %s", $newNFC, $currSID);
+  $updateSql = ("UPDATE studentdata SET NFCID = %s WHERE StudentID = %s");
   $updateSql -> bind_param("ii", $_POST[newNFC], $_POST[currSID]);
   $currSID -> execute();
+  $currSID -> close();
+  
+  $currID -> execute();
+  $currID -> close();
+  
+  $currSID -> execute();
+  $currSID -> close();
+  
+  $newNFC -> execute();
+  $newNFC -> close();
   
   $fetchupdatesql = "SELECT Username, NFCID FROM studentdata";
   $fetchupdatesql = mysqli_query($conn, $sql);
@@ -58,9 +64,7 @@
      {
        echo "|The student whose NFCID needs replacement: ".$row['Username']."|The NFCID to be replaced:".$row['NFCID']."; \n";
      }
-	 
-	 <!-- Unsure about this bit below too, has to be tested -->
-	 
+ 
 	 while($row = mysqli_fetch_assoc($fetchupdatesql))
      {
        echo "|There was a new NFC ID update to the student with the SID: ".$row['Username']."|The new NFC tag code assigned :".$row['NFCID']."; \n";
